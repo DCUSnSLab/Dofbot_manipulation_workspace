b15f9de0321726a8d244f4ab975968f421b9d537
choeyujeong911 <yujeongchoe20@gmail.com>
2025-01-15 09:39:38 +0900

    Delete useless packages

--- dofbot_ws/src/dofbot_color_identify/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youjeong/Dofbot_manipulation_workspace/dofbot_ws/src/dofbot_color_identify
-# Build directory: /home/youjeong/Dofbot_manipulation_workspace/dofbot_ws/src/dofbot_color_identify
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
