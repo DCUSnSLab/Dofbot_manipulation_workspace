12e192e55b2f41258ff76846d4e1daf972cb9856
choeyujeong911 <yujeongchoe20@gmail.com>
2025-01-15 10:15:52 +0900

    .

--- dofbot_ws/build/zed-ros-wrapper/zed_wrapper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youjeong/Dofbot_manipulation_workspace/dofbot_ws/src/zed-ros-wrapper/zed_wrapper
-# Build directory: /home/youjeong/Dofbot_manipulation_workspace/dofbot_ws/build/zed-ros-wrapper/zed_wrapper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
