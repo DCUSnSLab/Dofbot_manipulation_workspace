cc39d9ff7faaea728823c1e550e61fee69e9f2c9
choeyujeong911 <yujeongchoe20@gmail.com>
2025-01-15 09:25:24 +0900

    Delete useless directories(Arm)

--- Arm/YahboomArm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source /home/dofbot/catkin_ws/devel/setup.bash
-cd /home/dofbot/Arm/
-python3 /home/dofbot/Arm/YahboomArm.pyc
--- Arm/jupyter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-source /home/dofbot/dofbot_ws/devel/setup.bash
-
-jupyter lab --port 8888 --ip=0.0.0.0
