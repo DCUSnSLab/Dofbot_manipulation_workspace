6f9edf404b2af0ad4b4cae4d34e39576c8a18e7f
choeyujeong911 <yujeongchoe20@gmail.com>
2025-01-15 10:21:17 +0900

    .

---
+++ dofbot_ws/build/zed-ros-wrapper/zed_nodelets/setup_custom_pythonpath.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
+
+PYTHONPATH=/home/youjeong/Dofbot_manipulation_workspace/dofbot_ws/devel/lib/python3/dist-packages:$PYTHONPATH
+exec /usr/bin/python3 "$@"
